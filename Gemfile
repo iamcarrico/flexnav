a47a963e2af275e918434d749623800bf9b362fa
Ian Carrico <Ian@FourKitchens.com>
2013-11-15 16:52:43 -0600

    Add things that I use to work

---
+++ Gemfile
@@ -0,0 +1,6 @@
+# Pull gems from RubyGems
+source 'https://rubygems.org'
+
+gem 'sass', '3.3.0.rc.1'
+gem 'compass', '0.13.alpha.10'
+gem 'breakpoint', '~>2.0'
--- Gruntfile.js
+++ Gruntfile.js
@@ -1,64 +1,99 @@
 /*global module:false*/
 module.exports = function(grunt) {
-	'use strict';
+  'use strict';
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        sass: {
-            dist: {
-                files: {
-                    'css/flexnav.css': 'sass/flexnav.scss',
-                    'css/page.css': 'sass/page.scss'
-                }
-            }
-        },
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= pkg.repository %> <%= pkg.license %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-            },
-            build: {
-                src: 'js/jquery.flexnav.js',
-                dest: 'js/jquery.flexnav.min.js'
-            }
-        },		
-        coffee: {
-            compile: {
-                files: {
-                    'js/jquery.flexnav.js': 'coffeescripts/jquery.flexnav.coffee'
-                }
-            }
-        },
-        coffeelint: {
-            files: ['coffeescripts/*.coffee'],
-            options: {
-              'no_trailing_whitespace': {
-                  'level': 'error'
-              },
-              'max_line_length': {
-                  'level': 'ignore'
-              }
-          }
-        },
-        watch: {
-            coffee: {
-              files: ['<%= coffeelint.files %>'],
-              tasks: ['coffeelint', 'coffee']
-            },
-            sass: {
-              files: ['sass/*.scss'],
-              tasks: 'sass'
-            }
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      options: {
+        livereload: 9005
+      },
+      css: {
+        files: ['sass/{,**/}*.scss'],
+        tasks: ['compass:dev']
+      },
+      coffee: {
+        files: ['<%= coffeelint.files %>'],
+        tasks: ['coffeelint', 'coffee', 'uglify']
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= pkg.repository %> <%= pkg.license %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'js/jquery.flexnav.js',
+        dest: 'js/jquery.flexnav.min.js'
+      }
+    },
+
+    coffee: {
+      compile: {
+        files: {
+          'js/jquery.flexnav.js': 'coffeescripts/jquery.flexnav.coffee'
         }
-    });
+      }
+    },
 
-    // Load necessary plugins
-    grunt.loadNpmTasks('grunt-coffeelint');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-coffee');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-    
-    grunt.registerTask('build', ['uglify']);
-    grunt.registerTask('test', ['coffeelint']);
-	grunt.registerTask('default', ['sass', 'coffee', 'uglify', 'watch']);
+    coffeelint: {
+      files: ['coffeescripts/*.coffee'],
+        options: {
+        'no_trailing_whitespace': {
+          'level': 'error'
+        },
+        'max_line_length': {
+          'level': 'ignore'
+        }
+      }
+    },
+
+    compass: {
+      options: {
+        config: 'config.rb',
+        bundleExec: true
+      },
+      dev: {
+        options: {
+          environment: 'development'
+        }
+      },
+      dist: {
+        options: {
+          environment: 'production',
+          force: true
+        }
+      }
+    },
+
+    devserver: {
+      server: {},
+      options: {
+        port: 8080,
+      }
+    },
+
+    concurrent: {
+      server: {
+        tasks: ['devserver', 'watch'],
+        options: {
+          logConcurrentOutput: false
+        }
+      }
+    },
+  });
+
+  // Load necessary plugins
+  grunt.loadNpmTasks('grunt-coffeelint');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-devserver');
+  grunt.loadNpmTasks('grunt-concurrent');
+
+  grunt.registerTask('build', ['uglify']);
+  grunt.registerTask('test', ['coffeelint']);
+	grunt.registerTask('default', ['compass:dev', 'coffee', 'uglify', 'concurrent:server']);
 };
